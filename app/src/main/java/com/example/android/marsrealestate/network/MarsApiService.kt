497ae751c613574457a0718826531e331c26fb4f
aaq-and-dev <aaronquaday@gmail.com>
2020-12-25 15:09:50 -0800

    ** declares enum class for options menu Filter ** refines getProperties in MarsApiService.kt to take a @Query parameter ** edits getMarsRealEstateProperties in OverviewViewModel.kt to accept Filter, init to SHOW_ALL ** creates updateFilter() fun in OverviewViewModel.kt ** handles options menu item clicks to call viewModel.updateFilter() fun

--- app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
+++ app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
@@ -26,6 +26,9 @@ import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.converter.scalars.ScalarsConverterFactory
 import retrofit2.create
 import retrofit2.http.GET
+import retrofit2.http.Query
+
+enum class MarsApiFilter(val value: String){SHOW_RENT("rent") , SHOW_BUY("buy"), SHOW_ALL("all")}
 
 private const val BASE_URL = "https://mars.udacity.com/"
 
@@ -42,7 +45,7 @@ private val retrofit = Retrofit.Builder()
 interface MarsApiService{
 
     @GET("realestate")
-    suspend fun getProperties(): List<MarsProperty>
+    suspend fun getProperties(@Query("filter") type: String): List<MarsProperty>
 
     object MarsApi{
         val retrofitService: MarsApiService by lazy{
--- app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
+++ app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
@@ -21,6 +21,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.android.marsrealestate.network.MarsApiFilter
 import com.example.android.marsrealestate.network.MarsApiService
 import com.example.android.marsrealestate.network.MarsProperty
 import kotlinx.coroutines.launch
@@ -54,18 +55,22 @@ class OverviewViewModel : ViewModel() {
      * Call getMarsRealEstateProperties() on init so we can display status immediately.
      */
     init {
-        getMarsRealEstateProperties()
+        getMarsRealEstateProperties(MarsApiFilter.SHOW_ALL)
 
+    }
+
+    fun updateFilter(filter: MarsApiFilter){
+        getMarsRealEstateProperties(filter)
     }
 
     /**
      * Sets the value of the status LiveData to the Mars API status.
      */
-    private fun getMarsRealEstateProperties() {
+    private fun getMarsRealEstateProperties(filter: MarsApiFilter) {
         viewModelScope.launch {
             _status.value = MarsApiStatus.LOADING
             try{
-                val listRes = MarsApiService.MarsApi.retrofitService.getProperties()
+                val listRes = MarsApiService.MarsApi.retrofitService.getProperties(filter.value)
                 _properties.value = listRes
                 _status.value = MarsApiStatus.DONE
             } catch (e : Exception){
